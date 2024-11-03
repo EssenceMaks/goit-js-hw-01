1f97240fee8e1b292f59d4b2bc332c7ba603ce01
Maxim_Kyianytsia <MaksKi.Wish@gmail.com>
2024-11-03 14:39:13 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -13,7 +13,7 @@ console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
 
-// Виклик на стрінку
+// Виклик на сторінку
 document.addEventListener('DOMContentLoaded', () => {
     const result3 = document.getElementById('result3');
     result3.innerHTML = `
